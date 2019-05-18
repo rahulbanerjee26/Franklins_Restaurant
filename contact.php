d3066b398966d6912b22ce3da002361ffcd7beb3
Rahul Banerjee <40443639+rahulbanerjee26@users.noreply.github.com>
2019-05-19 01:31:35 +0300

    Update contact.php
    Updated Contact Form

--- contact.php
+++ contact.php
@@ -5,44 +5,66 @@ include("header.php")
 ?>
 
 <div id="contact">
-    <hr>
-    <h2> Get in Touch with us </h2>
+		<hr>
+		
+		<h1>Get in touch with us!</h1>
+		
 
-    <?php
-        if(isset($_POST["contact_submit"])) {
-            $name = $_POST["name"];
-            $email = $_POST["email"];
-            $message = $_POST["message"];
-            $to = "rahul2699@yahoo.com";
-            $subject = "$name has sent a message";
-            $message = "$message";
-            $headers ="";
+		<?php
+		
+		
+		if (isset($_POST['contact_submit'])) {
+			$name	= trim($_POST['name']);
+			$email	= trim($_POST['email']);
+			$msg	= $_POST['message'];
+			
+			if (!$name || !$email || !$msg) {
+				echo '<h4 class="error">All fields required.</h4><a href="contact.php" class="button block">Go back and try again</a>';
+				exit;
+			}
+			
+			$to	= "rahul2699@yahoo.com";
+			
+			$subject = "$name sent a message via your contact form";
 
-            mail($to,$subject,$message,$headers);
+			$message .= "Name: $name\r\n";
+			$message .= "Email: $email\r\n\r\n";
+			$message .= "Message:\r\n$msg";
 
-        }
-    else {
-    ?>
+			$headers = "MIME-Version: 1.0\r\n";
+			$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+			$headers .= "From: " . $name . " <" . $email . ">\r\n";
+			$headers .= "X-Priority: 1\r\n";
+			$headers .= "X-MSMail-Priority: High\r\n\r\n";
+			mail($to, $subject, $message, $headers);
+		?>
+		
+		<h5>Thanks for contacting Franklin's!</h5>
+		<p>Please allow 24 hours for a response.</p>
+		<p><a href="/final" class="button block">&laquo; Go to Home Page</a></p>
+		
+		<?php
+			} else {
+		?>
 
-    <form method="post" id="contact-form" action="thanks.php">
-        <label for="name">Your Name</label>
-        <input type="text" id="name" name="name">
-
-        <label for="email"> Your Email </label>
-        <input type="text" id="email" name="email">
-
-        <label for="message"> Your Message </label>
-        <input type="text" id="message" name="message">
-
-        <input type="checkbox" id="subscribe" name="subscribe" value="Subscribe">
-        <label for="subscribe">Subscribe to our NewsLetters</label>
-
-        <input type="submit" class="button next" name="contact_submit" value="Send Message">
-
-    </form>
-</div>
-
-<?php
-    }
-?>
+		<form method="post" action="" id="contact-form">
+			<label for="name">Your name</label>
+			<input type="text" id="name" name="name">
+			
+			<label for="email">Your email</label>
+			<input type="email" id="email" name="email">
+			
+			<label for="message">and your message</label>
+			<textarea id="message" name="message"></textarea>
+			
+			<input type="checkbox" id="subscribe" value="Subscribe" name="subscribe"> <label for="subscribe">Subscribe to newsletter</label>
+			
+			<input type="submit" class="button next" name="contact_submit" value="Send Message">
+		</form>
+		
+		<?php
+			}
+		?>
+		<hr>
+	</div>
 <?php include("footer.php")?>
